b9ffe6417195fe501c5a5b90068d2c7f255a3b52
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-07-07 19:52:15 +0200

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -927,7 +927,7 @@ void onLaunch(App* app)
     // create primitives
     app->cubeWithoutUV = createCubeWithoutUV(app->device);
     app->cube = createCube(app->device);
-    app->roundedCube = createRoundedCube(app->device, simd_float3{1.0f, 2.0f, 3.0f}, 0.2f, 3);
+    app->roundedCube = createRoundedCube(app->device, simd_float3{2.0f, 2.0f, 2.0f}, 0.5f, 3);
     app->sphere = createSphere(app->device, 60, 60);
     app->plane = createPlane(app->device, RectMinMaxf{-30, -30, 30, 30});
 
@@ -1176,7 +1176,7 @@ void drawScene(App* app, id <MTLRenderCommandEncoder> encoder, DrawSceneFlags_ f
 
     // draw rounded cube
     {
-        [encoder setCullMode:MTLCullModeNone];
+        [encoder setCullMode:MTLCullModeBack];
         [encoder setTriangleFillMode:MTLTriangleFillModeLines];
         [encoder setRenderPipelineState:(flags & DrawSceneFlags_IsShadowPass) ? app->shadowShader : app->litShader];
         [encoder setDepthStencilState:app->depthStencilStateDefault];
