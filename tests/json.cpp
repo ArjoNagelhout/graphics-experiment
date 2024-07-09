b7d0129ce304d52ef6eee724de76f8085e881e48
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-07-09 18:36:04 +0200

    add testing framework

---
+++ tests/json.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arjo Nagelhout on 09/07/2024.
+//
+
+#include "gtest/gtest.h"
---
+++ tests/websockets.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Arjo Nagelhout on 09/07/2024.
+//
+
+#include "gtest/gtest.h"
+
+namespace websockets_test
+{
+    TEST(Websocket, Test)
+    {
+        
+    }
+}
