d7a296a5542a671691aa25fd195a66178c7200c5
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-08-20 16:08:52 +0200

    add molten vk test

--- tests/molten_vk.cpp
+++ tests/molten_vk.cpp
@@ -2,3 +2,11 @@
 // Created by Arjo Nagelhout on 17/08/2024.
 //
 
+#include <iostream>
+
+#include <MoltenVK/mvk_vulkan.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "hello world" << std::endl;
+}
