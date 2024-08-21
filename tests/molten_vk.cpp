987a02725a3c317f6f7c7e44d29f80a2c3161525
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-08-21 14:47:13 +0200

    create vulkan instance using vulkan hpp headers
    let's try to use as little boiler plate as possible. vulkan.hpp helps with this

--- tests/molten_vk.cpp
+++ tests/molten_vk.cpp
@@ -5,55 +5,88 @@
 #include <iostream>
 #include <cassert>
 
-#include <MoltenVK/mvk_vulkan.h>
+#define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
+#define VULKAN_HPP_RAII_NO_EXCEPTIONS
+#define VULKAN_HPP_NO_EXCEPTIONS
+#define VK_ENABLE_BETA_EXTENSIONS 1 // for VK_KHR_portability_enumeration
+
+#include <vulkan/vulkan_raii.hpp>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
 
 struct App
 {
     // vulkan handles are simply pointers, do not contain any data
     // retrieving data needs to be done using functions
-    VkInstance_T* instance;
-    VkPhysicalDevice_T* physicalDevice;
-    VkPhysicalDeviceProperties properties;
-    VkDevice_T* device;
+    vk::raii::Context context = &vkGetInstanceProcAddr;
+//    vk::raii::Instance instance = nullptr;
+//    vk::raii::PhysicalDevice physicalDevice = nullptr;
+//    VkPhysicalDeviceProperties properties;
+//    VkDevice_T* device;
 };
 
 int main(int argc, char** argv)
 {
-    App app{};
+    // initialize sdl
+    {
+        int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+        assert(result >= 0);
+    }
+
+    App app;
+    uint32_t version = app.context.enumerateInstanceVersion();
+    std::cout << "version: " << version << std::endl;
 
     // create instance
+    if (0)
     {
-        VkApplicationInfo appInfo{
-            .apiVersion = VK_API_VERSION_1_2
-        };
+//        uint32_t count = 0;
+//        char const* const* extensions = SDL_Vulkan_GetInstanceExtensions(&count);
+//
+//        for (int i = 0; i < count; i++)
+//        {
+//            std::cout << extensions[i] << std::endl;
+//        }
 
-        VkInstanceCreateInfo info{
-            .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-            .pApplicationInfo = &appInfo
-        };
-        VkResult result = vkCreateInstance(&info, nullptr, &app.instance);
-        assert(result == VK_SUCCESS);
+        //std::vector<vk::ExtensionProperties> supportedExtensions = app.context.enumerateInstanceExtensionProperties(nullptr);
+
+        vk::ApplicationInfo appInfo(
+            "App",
+            {},
+            {},
+            {},
+            vk::ApiVersion13
+        );
+        vk::InstanceCreateInfo info(
+            {},
+            &appInfo,
+            0,
+            nullptr,
+            0,
+            nullptr
+        );
+        //app.instance = app.context.createInstance(info).value();
     }
 
     // get physical device
     {
-        uint32_t count = 0;
-        vkEnumeratePhysicalDevices(app.instance, &count, nullptr);
-        assert(count > 0);
-        std::vector<VkPhysicalDevice_T*> devices(count);
-        vkEnumeratePhysicalDevices(app.instance, &count, devices.data());
-        app.physicalDevice = devices[0];
-
-        vkGetPhysicalDeviceProperties(app.physicalDevice, &app.properties);
+//        uint32_t count = 0;
+//        vkEnumeratePhysicalDevices(app.instance, &count, nullptr);
+//        assert(count > 0);
+//        std::vector<VkPhysicalDevice_T*> devices(count);
+//        vkEnumeratePhysicalDevices(app.instance, &count, devices.data());
+//        app.physicalDevice = devices[0];
+//
+//        vkGetPhysicalDeviceProperties(app.physicalDevice, &app.properties);
     }
 
     // create device
     {
-        VkDeviceCreateInfo info{
-            .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
-        };
-        VkResult result = vkCreateDevice(app.physicalDevice, &info, nullptr, &app.device);
-        assert(result == VK_SUCCESS);
+//        VkDeviceCreateInfo info{
+//            .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+//        };
+//        VkResult result = vkCreateDevice(app.physicalDevice, &info, nullptr, &app.device);
+//        assert(result == VK_SUCCESS);
     }
 
     std::cout << "hello world" << std::endl;
